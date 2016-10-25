bb3f24aec9fa541f01baf8e17a1349204574714c
deeskr <deeskr@users.noreply.github.com>
2016-10-25 12:55:36 -0400

    Add files via upload

--- client-object.js
+++ client-object.js
@@ -1,156 +1,47 @@
-/* 
+/*******************************************************************
  * Implemntation of multi-user X3DOM - client side.
  * author: Matthew Bock
  * This version focuses on minimizing data transfer, but it still
  * sends and receives updates as soon as they happen with no regard
  * to time since the last update.
- */
-
+ *
+ * edited by: Karsten Dees, Nick Hu {09/16/2016}
+ ******************************************************************/
+ 
+//-----------------------------
+// Data Fields
+//-----------------------------
+ 
 var name;
 var socket;
-var spawnPosition = {"x":2, "y":1.5, "z":9};
-var spawnOrientation = [{"x":0, "y":1, "z":0}, 0];
+var spawnPosition = {"x": 2, "y": 1.5, "z": 1};
+var spawnOrientation = [{"x": 0, "y": 1, "z": 0}, 0];
 var x3d;
-var allowuUpdate = false;
+var bundleObj;
 
+//Use for localhost testing. Run node server 
+socket = new io.connect('http://metagrid2.sv.vt.edu:8888');
 
-function x3domWebsocketClient(sync, newUser, removeUser, update)
+/*
+ * Initialized by client.js to get the user's name
+ * and set up the scene
+ */
+function x3domWebsocketClient()
 {
 	name = prompt("Enter your name:");
 
 	x3d = document.getElementsByTagName("X3D")[0];
-
+	
 	configureScene();
-	
-	//socket = new io.connect('http://dev.mirrorworlds.icat.vt.edu:9000');
-	//Use to connect to mirrorworlds server
-	//socket = new io.Socket("http://dev.mirrorworlds.icat.vt.edu:8888",{ port: 8888 });
-	//Use for localhost testing. Run node server  
-	socket = new io.connect('http://metagrid2.sv.vt.edu:8888');
-	socket.connect();
-	
-	socket.on('connect', function()
-		{
-		    console.log("ConnectFunction");
-			// Tell the server to add this client to the list of all clients.
-			socket.emit('newconnection');
-		});
-
-	socket.on('firstupdate', function(data)
-		{
-			// Recieved the first time this client connects to the server.
-			// Gets the client up to speed with all of the current data.
-
-			// Update the scene.
-			console.log("1stUpdate");
-			var hook = document.getElementById("avatarGroup");
-			hook.innerHTML = "";
-			for (var key in data)
-			{
-				var current = data[key];
-				var t = document.createElement('Transform');
-				t.setAttribute("translation", current[1].x + " " + current[1].y + " " + current[1].z);
-				t.setAttribute("rotation", current[2][0].x + " " + current[2][0].y + " " + current[2][0].z + " " + current[2][1]);
-				t.setAttribute("id", key + "Avatar");
-				console.log("created Node: " + t.getAttribute("id"));
-				var i = document.createElement('inline')			
-				i.setAttribute("url", "content/avatars/redBox.x3d")
-	
-				t.appendChild(i);
-				hook.appendChild(t);
-			}
-
-			// Send the data to the callback
-			if(sync != null)
-			{
-				sync(data);
-			};
-
-			socket.emit('login', name, spawnPosition, spawnOrientation);
-		});
-
-	socket.on('update', function(data)
-		{
-			console.log("Updated");
-			// Update the scene
-			var t = document.getElementById(data[0] + "Avatar");
-			
-			if(t != null)
-			{
-				t.setAttribute("translation", data[1].x + " " + data[1].y + " " + data[1].z);
-				t.setAttribute("rotation", data[2][0].x + " " + data[2][0].y + " " + data[2][0].z + " " + data[2][1]);
-			};
-
-			if (update != null)
-			{
-				update(data);
-			};
-		});
-
-	socket.on('newuser', function(data)
-		{
-			console.log("NewUser");
-			if(data[0] != name)
-			{
-				// Update the scene.
-				var hook = document.getElementById("avatarGroup");
-
-				var t = document.createElement('Transform');
-
-				t.setAttribute("translation", data[1].x + " " + data[1].y + " " + data[1].z);
-				t.setAttribute("rotation", data[2][0].x + " " + data[2][0].y + " " + data[2][0].z + " " + data[2][1]);
-				t.setAttribute("id", data[0] + "Avatar");
-
-				console.log("Created node: " + t.getAttribute("id"));
-
-				var i = document.createElement('inline');		
-				i.setAttribute("url", "content/avatars/redBox.x3d");
-
-				t.appendChild(i);
-				hook.appendChild(t);
-
-				if (newUser != null)
-				{
-					newUser(data);
-				};
-				// Update the list of users.
-//				var userList = document.getElementById("users");
-//				var userListEntry = document.createElement('span');
-//				var br = document.createElement('br');
-//				userListEntry.setAttribute("id", data[0]);
-//				userListEntry.innerHTML = (data[0] + " observing at: " + data[1].x + ", " + data[1].y + ", " + data[1].z);
-//
-//				userList.appendChild(br);
-//				userList.appendChild(userListEntry);
-			}
-
-			allowuUpdate = true;
-
-		});
-
-	socket.on('deleteuser', function(data)
-		{
-			console.log("UserDeleted");
-			// Remove the avatar from the scene.
-			var avatars = document.getElementById("avatarGroup");
-			var remove1 = document.getElementById(data[0] + "Avatar");
-			avatars.removeChild(remove1);
-			
-			if (removeUser != null)
-			{
-				removeUser(data);
-			};
-			// Remove the user from the list of users.
-//			var users = document.getElementById("users");
-//			var remove2 = document.getElementById(name);
-//			users.removeChild(remove2);
-
-		});
 }
 
+/*
+ * Sets up the X3D scene
+ */
 function configureScene()
 {
 	console.log("Scene Configured");
+	bundleObj = [name, spawnPosition, spawnOrientation];
 	var camera = x3d.runtime.getActiveBindable("Viewpoint");
 	var cPos = "" + spawnPosition.x + " " + spawnPosition.y + " " + spawnPosition.z;
 	var cRot = "" + spawnOrientation[0].x + " " + spawnOrientation[0].y + " " + spawnOrientation[0].z + " " + spawnOrientation[1];
@@ -167,12 +58,206 @@ function configureScene()
 	};
 }
 
+/*
+ * Sends position data to server
+ */
 function positionUpdated(e)
-{			
-	var pos = e.position;
+{	
+    var pos = e.position;
     var rot = e.orientation;
-    //console.log([name, pos, rot]);
+    
     //Tell the server that this client has moved and send its new location data
-
 	socket.emit('updateposition', name, pos, rot);
 }
+
+//-----------------------------
+// Listeners
+//-----------------------------
+
+window.addEventListener('keypress', function(e) {
+	
+	var avatar = document.getElementById(name + "Avatar");
+	
+	//pressed 1
+	if(e.keyCode === 49) {
+		
+		console.log("Change to First Person View");
+		avatar.setAttribute("translation", "" + 0 + " " + 0 + " " + 5);
+	}
+	//pressed 3
+	if(e.keyCode === 51) {
+		
+		console.log("Change to Third Person View");
+		var zPosition = -5;
+		
+		avatar.setAttribute("translation", "" + 0 + " " + 0 + " " + zPosition);
+	}
+});
+
+/*
+ * Tell the server the user has successfully connected and 
+ *to add this client to the list of all clients
+ */
+socket.on('connect', function()
+{
+	socket.emit('newconnection');
+});
+
+/*
+ * Received the first time this client connects to the server -- gets 
+ * the client up to speed with all of the current data
+ *
+ * @param fullListOfUsers - the list of connected users
+ */
+socket.on('firstupdate', function(fullListOfUsers)
+{
+	//Add your own Name and information to fullListOfUsers
+	if(fullListOfUsers[0] === undefined) {
+		
+		fullListOfUsers[name] = bundleObj;
+	
+	}
+	
+	// Adds Avatar to X3D scene for new user
+	var avatarGroup = document.getElementById("avatarGroup");
+	avatarGroup.innerHTML = "";
+	
+	var scene = document.getElementsByTagName("Scene")[0];
+	for (var key in fullListOfUsers)
+	{
+		var current = fullListOfUsers[key];
+		//Adding yourself to the Scene
+	
+		var userAvatar = document.createElement('Transform');
+			
+		userAvatar.setAttribute("translation", "" + 0 + " " + 0 + " " + 5);
+		userAvatar.setAttribute("rotation", "" + 0 + " " + 0 + " " + 0 + " " + 0);
+		userAvatar.setAttribute("id", key + "Avatar"); 
+		console.log("created Node: " + userAvatar.getAttribute("id"));
+		var characterOfAvatar = document.createElement('inline');
+		characterOfAvatar.setAttribute("url", "avatars/pumbaPTrans1.x3d");
+			
+		userAvatar.appendChild(characterOfAvatar);
+
+		console.log("Name Actual: ", current[0]);
+		console.log("Name Expected: ", current[0]);
+		
+		if(current[0] == name) {
+			console.log("Creating my own UserBundle");
+			var userBundle = document.createElement('Transform');
+			userBundle.setAttribute("id", key + "Bundle");
+			userBundle.setAttribute("translation", current[1].x + " " + current[1].y + " " + current[1].z);
+			userBundle.setAttribute("rotation", current[2][0].x + " " + current[2][0].y + " " + current[2][0].z + " " + current[2][1]);
+			
+			var scene = document.getElementsByTagName("Scene")[0];
+			
+			scene.appendChild(userBundle);
+			userBundle.appendChild(userAvatar);
+
+		} else {
+			avatarGroup.appendChild(userAvatar)
+		}
+	}
+    
+	//Build the list of connected users
+	buildList(fullListOfUsers);
+	
+	//Tell the server the user's spawn location data
+	socket.emit('login', name, spawnPosition, spawnOrientation);
+});
+
+/*
+ * Triggered whenever the user changes location 
+ * to update the X3D scene and the HTML tags
+ *
+ * @param updateUser - the updated user
+ */
+socket.on('update', function(updatedUser)
+{
+	//Get User Bundle Data
+	var userBundle = document.getElementById(updatedUser[0] + "Bundle");
+	
+	// Get Avatar Data
+	var userAvatar = document.getElementById(updatedUser[0] + "Avatar");
+	
+	if(userBundle != null)
+	{
+		//Update Bundle Data
+		userBundle.setAttribute("translation", updatedUser[1].x + " " + updatedUser[1].y + " " + updatedUser[1].z);
+		userBundle.setAttribute("rotation", updatedUser[2][0].x + " " + updatedUser[2][0].y + " " + updatedUser[2][0].z + " " + updatedUser[2][1]);
+				
+        //Update HTML
+        updateList(updatedUser);
+    
+        //Update Server Location Information
+        socket.emit('updatePosition', updatedUser[0], updatedUser[1], updatedUser[2]);
+	} else if(userAvatar != null)
+	{
+	
+		//Update Avatar Data
+		userAvatar.setAttribute("translation", updatedUser[1].x + " " + updatedUser[1].y + " " + updatedUser[1].z);
+		userAvatar.setAttribute("rotation", updatedUser[2][0].x + " " + updatedUser[2][0].y + " " + updatedUser[2][0].z + " " + updatedUser[2][1]);
+		
+        //Update HTML
+        updateList(updatedUser);
+    
+        //Update Server Location Information
+        socket.emit('updatePosition', updatedUser[0], updatedUser[1], updatedUser[2]);
+	}
+});
+
+/*
+ * Triggered whenever a new user connects -- updates 
+ * all users with the added user's information
+ *
+ * @param newestUser - the updated user
+ * @param fullListOfUsers - all of the connected users
+ */
+socket.on('newuser', function(newestUser)
+{	
+	var duplicateNames = document.getElementById(newestUser[0]);
+	
+	if(newestUser[0] != null && name != newestUser[0] && duplicateNames == null)
+	{	
+		//Add Users Avatar
+        var avatarGroup = document.getElementById("avatarGroup");
+
+		var userAvatar = document.createElement('Transform');
+
+		userAvatar.setAttribute("translation", newestUser[1].x + " " + newestUser[1].y + " " + newestUser[1].z);
+		userAvatar.setAttribute("rotation", newestUser[2][0].x + " " + newestUser[2][0].y + " " + newestUser[2][0].z + " " + newestUser[2][1]);
+		userAvatar.setAttribute("id", newestUser[0] + "Avatar");
+
+		console.log("Created node: " + userAvatar.getAttribute("id"));
+
+		var inlineElement = document.createElement('inline');
+		inlineElement.setAttribute("url", "avatars/pumbaPTrans1.x3d");
+		
+		userAvatar.appendChild(inlineElement);
+		avatarGroup.appendChild(userAvatar);
+        
+		//Update HTML
+		addUser(newestUser);
+	}
+});
+
+/*
+ * Triggered whenever a user disconnects -- removes
+ * the deleted user from everyone else's list
+ *
+ * @param removableUser - the user to be deleted
+ */
+socket.on('deleteuser', function(removableUser) 
+{
+	// Remove the avatar from the scene.
+	var removeAvatar = document.getElementById(removableUser[0] + "Avatar");
+
+	if(removeAvatar != null)
+	{
+		var avatars = document.getElementById("avatarGroup");
+		avatars.removeChild(removeAvatar);
+	}
+    
+    //Remove User's HTML Content
+    removeUser(removableUser);
+});
